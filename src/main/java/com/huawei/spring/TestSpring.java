51332b588ddeb092b48b587bac866ef98adad397
yuanlibo <936576946@qq.com>
2021-05-18 17:49:30 +0800

    ignore-commit

--- src/main/java/com/huawei/spring/TestSpring.java
+++ src/main/java/com/huawei/spring/TestSpring.java
@@ -3,5 +3,6 @@ package com.huawei.spring;
 public class TestSpring {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("java");
     }
 }
