6f5b5a607d27884bb7689c3ec9bedf7f1f2541ab
yuanlibo <936576946@qq.com>
2021-05-18 19:00:32 +0800

    aaaa

--- src/main/java/com/huawei/spring/TestSpring.java
+++ src/main/java/com/huawei/spring/TestSpring.java
@@ -4,5 +4,6 @@ public class TestSpring {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("java");
+	System.out.println("scala");
     }
 }
